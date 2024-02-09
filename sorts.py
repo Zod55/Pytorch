33427f3869245e0d5edda0a7b2ee8b641f6e1937
Zod55 <126630921+Zod55@users.noreply.github.com>
2024-02-09 22:35:13 +0200

    sorts.py

--- sorts.py
+++ sorts.py
@@ -8,8 +8,8 @@ import openpyxl
 
 #need to put the path to chrome find it later 
 driver = webdriver.Chrome()
-
-def get_account(): 
+def download_short(driver):
+def get_account(driver): 
 
  driver.get('https://internxt.com/temporary-email')
  # Find the div of the renemail
@@ -34,12 +34,14 @@ need to add code to press the link
 the YouTube api: 
 4. add a function that will put a video under the downloaded 
 video:
+
 """
+driver.switch_to.window(driver.window_handles[1])
 
 #we get in the ai shorts website
  driver.get("https://klap.app/login")
-
-# find the html to insert the link from the db
+# need to press the crate new account
+# find the html to insert the new account 
  insert_email = driver.find_element(_class="supabase-auth-ui_ui-input !rounded-xl c-dEnagJ c-dEnagJ-bBzSYw-type-default")
  insert_pass = driver.find_element(_class="supabase-auth-ui_ui-input !rounded-xl c-dEnagJ c-dEnagJ-bBzSYw-type-password")
 
@@ -47,12 +49,12 @@ video:
  insert_email.send_keys(rmemail)
  insert_pass.send_keys("123456789")
  insert_element.send_keys(Keys.RETURN)
- #need to add a code that will press the link on 
- driver.get('https://shorts-with-ai.com')
- 
-#the random email address website 
+ #need to add a code that will press the link 
+ driver.switch_to.window(driver.window_handles[0])
+ verification_link = driver.find_element_by_xpath("the class of the link" )
+ verification_link.click()
 
-def create_short(catgeroy):
+def create_short(catgeroy,driver):
      #open the excel 
      workbook = openpyxl.load_workbook('my_file.xlsx')
      #select the active sheet
@@ -60,38 +62,9 @@ def create_short(catgeroy):
      for cell in sheet[catagory]:
        
           insert_email = driver.find_element("""need to put the class for insert the link""")
-        
-          
-
-
-
-    # wait 10
-    time.sleep(600)
-
-
-
-# Open the first tab and navigate to the email service
-driver.get('https://your-email-service.com')
-
-# Open a new tab
-
-# Switch to the second tab
-driver.switch_to.window(driver.window_handles[1])
-
-# Navigate to the website that creates shorts with AI
-
-# Switch back to the first tab
-driver.switch_to.window(driver.window_handles[0])
-
-# Now you are back to the first tab
-
-
-
-
-
-
-
-
+          # wait 10 min
+          time.sleep(600)
+          download_short(driver)
 
 
 # Iterate over the cells in the first column and append data to the array
@@ -112,20 +85,4 @@ print(data_array)
 
 
 
-# Open a webpage
-driver.get('https://example.com')
 
-# Find the parent div element
-parent_div = driver.find_element_by_xpath('//div[@class="parent-class"]')
-
-# Find the <p> element inside the parent div
-p_element = parent_div.find_element_by_tag_name('p')
-
-# Extract the text from the <p> element
-text = p_element.text
-
-# Print the extracted text
-print(text)
-
-# Close the WebDriver
-driver.quit()
