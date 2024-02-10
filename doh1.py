4ae9436d06ab4499439f21723744b12a9140c9cd
Zod55 <126630921+Zod55@users.noreply.github.com>
2024-02-10 18:23:13 +0200

    Update doh1.py

--- doh1.py
+++ doh1.py
@@ -5,3 +5,7 @@ import time
 
 driver = webdriver.Chrome()
 driver.get('https://one.prat.idf.il/login') 
+
+insert_id = driver.find_element(_class ="formControl")
+insert_id.send_keys(input("whats your id:"))
+insert_id.send_keys(Keys.
