276e871f810bb56a070a6cd6799ddb5aa1f2b042
Zod55 <126630921+Zod55@users.noreply.github.com>
2024-02-10 18:56:41 +0200

    Update doh1.py

--- doh1.py
+++ doh1.py
@@ -8,4 +8,13 @@ driver.get('https://one.prat.idf.il/login')
 
 insert_id = driver.find_element(_class ="formControl")
 insert_id.send_keys(input("whats your id:"))
-insert_id.send_keys(Keys.
+insert_id.send_keys(Keys.RETURN)
+
+insert_pass = driver.find_element(_class="form-control input ext-input text-box ext-text-box")
+inert_pass = driver.send_keys(input("whats yout pass")
+insert_pass.send_keys(Keys.RETURN)
+"""
+1. need to see how we get in to the microsoft and take the code from the mail
+2. add the press the doh1
+3. send the like to the group chat
+"""
